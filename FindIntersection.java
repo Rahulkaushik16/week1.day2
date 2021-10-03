f2fee5e8b96e534b93b128ac821c18365ad49b1b
Kaushik Sudheer <34548691+Rahulkaushik16@users.noreply.github.com>
2021-10-03 21:11:06 +0530

    Add files via upload

--- FindIntersection.java
+++ FindIntersection.java
@@ -3,8 +3,8 @@ package week1.day2.assignments;
 public class FindIntersection {
 
 	public static void main(String[] args) {
-		int a[] = { 3, 2, 11, 4, 6, 7 };
-		int b[] = { 1, 2, 8, 4, 9, 7 };
+		int a[] = { 3, 2, 11, 4, 6, 7 }; // Declare An array for {3,2,11,4,6,7};
+		int b[] = { 1, 2, 8, 4, 9, 7 }; // Declare another array for {1,2,8,4,9,7};
 		int c = 0;
 		int len = a.length;
 		System.out.println("Length of an Array [a] is : " + len);
@@ -13,12 +13,12 @@ public class FindIntersection {
 
 		System.out.println("Intersection of Two Arrays : ");
 
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < b.length; j++) {
-				if (a[i] == b[j]) {
+		for (int i = 0; i < a.length; i++) { // Declare for loop iterator from 0 to array length
+			for (int j = 0; j < b.length; j++) { // Declare a nested for another array from 0 to array length
+				if (a[i] == b[j]) { // Compare Both the arrays using a condition statement
 					c = a[i];
 
-					System.out.println(c);
+					System.out.println(c); // Print the first array (shoud match item in both arrays)
 				}
 			}
 		}
